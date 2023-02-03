caaf3be94603ac99989189c8b88c16107e492ab2
Hossam <hossamibrahim66666@gmail.com>
2023-02-04 00:56:06 +0200

    last update

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Dashboard\AboutUsController;
+use App\Http\Controllers\Dashboard\ClinicProfileController;
+use App\Http\Controllers\Dashboard\TestimonialController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Auth::routes();
+
+
 Route::get('/', function () {
     return view('end_user.index');
 });
@@ -22,6 +28,34 @@ Route::get('/dashboard', function () {
 });
 
 
-Auth::routes();
+Route::group(['prefix' => 'about_us', 'as' => 'about_us.'], function () {
+    Route::get('/', [AboutUsController::class, 'index'])->name('index');
+    Route::get('/create', [AboutUsController::class, 'create'])->name('create');
+    Route::post('/store', [AboutUsController::class, 'store'])->name('store');
+    Route::get('edit/{id}', [AboutUsController::class, 'edit'])->name('edit');
+    Route::put('update/{id}', [AboutUsController::class, 'update'])->name('update');
+    Route::delete('destroy/{id}', [AboutUsController::class, 'destroy'])->name('destroy');
+});
+
+
+Route::group(['prefix' => 'clinic_profile', 'as' => 'clinic_profile.'], function () {
+    Route::get('/', [ClinicProfileController::class, 'index'])->name('index');
+    Route::get('/create', [ClinicProfileController::class, 'create'])->name('create');
+    Route::post('/store', [ClinicProfileController::class, 'store'])->name('store');
+    Route::get('edit/{id}', [ClinicProfileController::class, 'edit'])->name('edit');
+    Route::put('update/{id}', [ClinicProfileController::class, 'update'])->name('update');
+    Route::delete('destroy/{id}', [ClinicProfileController::class, 'destroy'])->name('destroy');
+
+});
+
+
+Route::group(['prefix' => 'testimonial', 'as' => 'testimonial.'], function () {
+    Route::get('/', [TestimonialController::class, 'index'])->name('index');
+    Route::get('edit/{id}', [TestimonialController::class, 'edit'])->name('edit');
+    Route::put('update/{id}', [TestimonialController::class, 'update'])->name('update');
+    Route::delete('destroy/{id}', [TestimonialController::class, 'destroy'])->name('destroy');
+});
+
+
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
